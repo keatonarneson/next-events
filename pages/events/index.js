3e999c94ba2445aedc93a285830fa39e29ff0f69
Keaton <keaton.arneson@gmail.com>
2022-07-28 14:38:23 -0500

    Initial commit

---
+++ pages/events/index.js
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { getAllEvents } from '../../helpers/api-util';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
+const AllEventsPage = ({ events }) => {
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const events = await getAllEvents();
+
+  return {
+    props: { events: events },
+    revalidate: 60,
+  };
+};
+
+export default AllEventsPage;
